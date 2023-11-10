3df216586ecde41bae9ae84ba6a2ad11b58f09e0
Tyler Skidmore <tyler.s9@proton.me>
2023-11-10 13:53:47 -0500

    built the form and completed code to append gifs to DOM

--- 19.3/ajax-giphy-party/app.js
+++ 19.3/ajax-giphy-party/app.js
@@ -1,1 +1,25 @@
-console.log("Let's get this party started!");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("#search-form");
+    form.addEventListener("submit", function (evt) {
+        evt.preventDefault();
+        const input = document.querySelector("#search");
+        const keyword = input.value;
+        searchGIF(keyword);
+        input.value = '';
+    });
+
+    async function searchGIF(keyword) {
+        const response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=fXuYPBplCR6wWFW3Vd2zDm0GpV7js8Oi`);
+        let results = response.data.data;
+        console.log(results[(Math.floor(Math.random() * results.length))].url);
+        let randomGifUrl = results[(Math.floor(Math.random() * results.length))].images.original.url;
+        appendGif(randomGifUrl);
+    }
+
+    function appendGif(source) {
+        const div = document.querySelector("#img-spot");
+        const img = document.createElement('img');
+        img.src = source;
+        div.append(img);
+    }
+});
