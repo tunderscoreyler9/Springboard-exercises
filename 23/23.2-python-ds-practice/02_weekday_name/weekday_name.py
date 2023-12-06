1d5295daf5394da07dce01514008973cc5800724
Tyler Skidmore <tyler.s9@proton.me>
2023-12-06 15:16:46 -0500

    first 8 practises completed

--- 23/23.2-python-ds-practice/02_weekday_name/weekday_name.py
+++ 23/23.2-python-ds-practice/02_weekday_name/weekday_name.py
@@ -12,3 +12,19 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+
+    if day_of_week < 1 or day_of_week > 7:
+        return None
+
+    return days[day_of_week - 1]
+
+    
