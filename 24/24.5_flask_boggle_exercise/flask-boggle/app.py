b7b907b3fd50c52798c204e966a6fd9945de1121
Tyler Skidmore <tyler.s9@proton.me>
2024-01-02 16:33:07 -0500

    added boggle exercise files

---
+++ 24/24.5_flask_boggle_exercise/flask-boggle/app.py
@@ -0,0 +1,4 @@
+from boggle import Boggle
+from flask import Flask
+
+boggle_game = Boggle()
---
+++ 24/24.5_flask_boggle_exercise/flask-boggle/test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from app import app
+from flask import session
+from boggle import Boggle
+
+
+class FlaskTests(TestCase):
+
+    # TOdo -- write tests for every view function / feature!
