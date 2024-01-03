f56b385255cc2cf885734e782a2e586b6ea873c4
Tyler Skidmore <tyler.s9@proton.me>
2024-01-03 10:59:59 -0500

    part 2 complete

--- 24/24.5_flask_boggle_exercise/flask-boggle/app.py
+++ 24/24.5_flask_boggle_exercise/flask-boggle/app.py
@@ -1,4 +1,24 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint, choice, sample
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.debug = True
+toolbar = DebugToolbarExtension(app)
 boggle_game = Boggle()
+
+@app.route('/')
+def index():
+    boggle_game = Boggle()
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('index.html', board=board)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- 24/24.5_flask_boggle_exercise/flask-boggle/test.py
+++ 24/24.5_flask_boggle_exercise/flask-boggle/test.py
@@ -3,7 +3,10 @@ from app import app
 from flask import session
 from boggle import Boggle
 
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class FlaskTests(TestCase):
 
     # TOdo -- write tests for every view function / feature!
+
