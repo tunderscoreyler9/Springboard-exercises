0b8774980de7f264ae7fd7e45c7c12d391c30a58
Tyler Skidmore <tyler.s9@proton.me>
2024-03-11 20:44:43 -0400

    completed part 2, GET cupcake by id and POST request for creating cupcakes

--- 29/29.3/flask-cupcakes/app.py
+++ 29/29.3/flask-cupcakes/app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 from flask import Flask, request, jsonify, render_template
-from models import db, connect_db, Cupcake
+from models import db, connect_db, Cupcake, DEFAULT_IMAGE
 
 app = Flask(__name__)
 
@@ -19,3 +19,26 @@ def list_cupcakes():
     """Returns JSON w/ all cupcakes"""
     all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
     return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:id>', methods=["GET"])
+def get_cupcake(id):
+    """Returns JSON for one cupcake in particular"""
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """Creates a new cupcake and returns JSON of that created cupcake"""
+    new_cupcake = Cupcake(
+        flavor=request.json["flavor"],
+        size=request.json["size"],
+        rating=request.json["rating"],
+        image=request.json.get("image", DEFAULT_IMAGE)
+    )
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+    
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    # Return a tuple
+    return (response_json, 201)
