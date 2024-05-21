f645c15e2101e240da0f1c9602ee4434fa053d81
Tyler Skidmore <tyler.s9@proton.me>
2024-05-21 03:49:01 -0400

    completed the exercise

--- 37/express-calculator/app.js
+++ 37/express-calculator/app.js
@@ -14,6 +14,28 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Mean:
+
+app.get('/mean', function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    // check if anything bad was put in
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+
+    let result = {
+        operation: "mean",
+        result: findMean(nums)
+    }
+
+    return res.send(result);
+});
+
 // Route to serve mean.html
 app.get('/mean', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'mean.html'));
@@ -39,6 +61,27 @@ app.post('/mean/post', (req, res, next) => {
     }
 });
 
+// Median: - - >
+app.get('/median', function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    // check if anything bad was put in
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+    let result = {
+        operation: "median",
+        result: findMedian(nums)
+    }
+
+    return res.send(result);
+
+});
+
 // Route to serve median.html
 app.get('/median', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'median.html'));
@@ -62,6 +105,28 @@ app.post('/median/post', (req, res, next) => {
     } catch (e) {
         return next(e);
     }
+});
+
+// Mode: - - >
+app.get('/mode', function (req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400);
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    // check if anything bad was put in
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+    let result = {
+        operation: "mode",
+        result: findMode(nums)
+    }
+
+    return res.send(result);
+
+
 });
 
 // Route to serve mode.html
